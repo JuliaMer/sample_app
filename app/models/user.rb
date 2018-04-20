8bf1a969ecab30f0493a482876aaf65fd2beda5c
Julia Merk <julia.merk@makandra.de>
2018-04-20 17:09:52 +0200

    added password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 255},
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensetive: false}
+  has_secure_password
+  validates :password, length: { minimum: 6 }
 end
